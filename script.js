eb1f7f2d4c2b99235a354a0936ee1694d9bca8a1
Foggles <jackfogarty2105@gmail.com>
2020-12-15 18:02:55 +0800

    Initial commit with pseudocoding

---
+++ script.js
@@ -0,0 +1,15 @@
+// TODO: Setup variables for queryURL and html elements.
+
+// TODO: City search functionality - add the city name to the queryURL.
+// * Term entered into search bar is stored into query and a seperate variable for later use.
+// * Button has an event listener to trigger the functionality.
+
+// TODO: If query responds, grab specific data to print to the jumbotron and 
+// * Assign a colour to the UV index depending on the result.
+// TODO: to the 5-day forcast.
+// * Depending on the forecast returned, use a specific icon representation.
+
+
+// TODO: Store queryURL in local storage, with the city name displayed beneath
+// TODO: the search bar.
+// * Create new elements and tie the city name and its result to it.
