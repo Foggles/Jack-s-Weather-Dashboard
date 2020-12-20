a7ebe3687729844cd7790b4b2f77fdce03e08c13
Jack Fogarty <jackfogarty2105@gmail.com>
2020-12-20 17:45:16 +0800

    Added functionality to the search button, as well as starting the AJAX calls to the OpenWaterMap APIs

--- script.js
+++ script.js
@@ -2,6 +2,9 @@
 let citySearchInput = document.getElementById("citySearchInput");
 let previousSearches = document.getElementById("previousSearches");
 let fiveDayForecast = document.getElementById("fiveDayForecast");
+let searchButton = document.getElementById("searchButton");
+
+let apiKey = "fd2fcff01d8067db25ed7968bc9d3a62";
 
 // Variables for the City Info Card
 let cityInfoName = document.getElementById("cityName");
@@ -13,6 +16,43 @@ let cityInfoUVIndex = document.getElementById("cityUVIndex");
 // TODO: City search functionality - add the city name to the queryURL.
 // * Term entered into search bar is stored into query and a seperate variable for later use.
 // * Button has an event listener to trigger the functionality.
+
+searchButton.addEventListener("click", function (event) {
+    event.preventDefault();
+    
+    const searchedCity = citySearchInput.value;
+    console.log("You searched for " + searchedCity);
+
+    let queryURLCurrentTemp = "https://api.openweathermap.org/data/2.5/weather?q=" + searchedCity + "&appid=" + apiKey;
+    let queryURLForecast = "https://api.openweathermap.org/data/2.5/forecast?q=" + searchedCity + "&appid=" + apiKey;
+    let queryURLUVIndex = "https://api.openweathermap.org/data/2.5/uvi?lat={lat}&lon={lon}&appid=" + apiKey;
+
+    console.log("Forecast: " + queryURLForecast);
+    console.log("Current: " + queryURLCurrentTemp);
+    console.log("UV Index: " + queryURLUVIndex);
+    
+    $.ajax({
+        url: queryURLCurrentTemp,
+        method: "GET"
+    }).then(function(reponse) {
+        console.log(reponse);
+    });
+
+    $.ajax({
+        url: queryURLForecast,
+        method: "GET"
+    }).then(function(reponse) {
+        console.log(reponse);
+    });
+
+    $.ajax({
+        url: queryURLUVIndex,
+        method: "GET"
+    }).then(function(reponse) {
+        console.log(reponse);
+    });
+
+});
 
 // TODO: If query responds, grab specific data to print to the jumbotron and 
 // * Assign a colour to the UV index depending on the result.
